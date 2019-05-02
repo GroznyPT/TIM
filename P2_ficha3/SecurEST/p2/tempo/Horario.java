c63bec0ed6ba9610f80877ed283c4a297d2d2996
Luis Alves <luisfiliper.alves@gmail.com>
2019-05-02 10:15:31 +0100

    Ficha 3 - Horario Class - Done!

--- P2_ficha3/SecurEST/p2/tempo/Horario.java
+++ P2_ficha3/SecurEST/p2/tempo/Horario.java
@@ -1,9 +1,36 @@
 package p2.tempo;
 
+import java.util.ArrayList;
+
 public class Horario {
 
+    private ArrayList<Periodo> periodos = new ArrayList<Periodo>();
+
+
+    public Horario() {
+    }
+
+    public void addPeriodo(Periodo p) {
+        for (int i=periodos.size()-1; i>=0; i--){
+            if (periodos.get(i).interseta(p)){
+                p.junta(periodos.get(i));
+                periodos.remove(i);
+            }
+        }
+        periodos.add(p);
+    }
+    
+    public void removePeriodo(int idx) {
+        periodos.remove(idx);
+    }
 
     public boolean estaDentro(Hora h) {
-        return true;
+        for (Periodo p : periodos) {
+            if (p.estaDentro(h))
+                return true;
+        }
+        return false;
     }
+
+
 }
